652c1d3278118047c6c550a08a9c44cd19055708
Pethkar-Tushar-Narendra <pethkartusharnarendra@gmail.com>
2024-06-19 17:22:19 +0530

    Done with CRUD and error handling and observables

--- sevices/src/app/app.component.ts
+++ sevices/src/app/app.component.ts
@@ -7,12 +7,69 @@ import { DataService } from '../data.service';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  title = 'sevices';
-  constructor(private loggingService: DataService) {
-    console.log(this.loggingService.getData(), 'data of services');
-  }
+  public title = 'sevices';
+  public data: any;
+  public errorMessage: any;
+  constructor(private jsonPlaceholderService: DataService) {}
   ngOnInit() {
-    // Use the services
-    console.log(this.loggingService.getData(), 'data of services');
+    this.getPosts();
+  }
+  // Function to format item
+  formatItem(item: any): string {
+    return `ID: ${item.userId}, Name: ${item.id}, Description: ${item.title}, Description: ${item.body}`;
+  }
+
+  posts: any[] = [];
+  post: any = {};
+
+  getPosts(): void {
+    this.jsonPlaceholderService.getPosts().subscribe({
+      next: (v) => (this.posts = v),
+      error: (e) => (this.errorMessage = e.message),
+      complete: () => console.info('complete'),
+    });
+  }
+
+  getPost(id: number): void {
+    this.jsonPlaceholderService.getPost(id).subscribe({
+      next: (v) => (this.post = v),
+      error: (e) => (this.errorMessage = e.message),
+      complete: () => console.info('complete'),
+    });
+  }
+
+  createPost(): void {
+    const newPost = { title: 'foo', body: 'bar', userId: 1 };
+    this.jsonPlaceholderService.createPost(newPost).subscribe({
+      next: (v) => this.posts.unshift(v),
+      error: (e) => (this.errorMessage = e.message),
+      complete: () => console.info('complete'),
+    });
+  }
+
+  updatePost(id: number): void {
+    const updatedPost = { title: 'foo', body: 'bar', userId: 1 };
+    this.jsonPlaceholderService.updatePost(id, updatedPost).subscribe({
+      next: (v) => (this.post = v),
+      error: (e) => (this.errorMessage = e.message),
+      complete: () => console.info('complete'),
+    });
+  }
+
+  patchPost(id: number): void {
+    const patchedPost = { title: 'foo' };
+    this.jsonPlaceholderService.patchPost(id, patchedPost).subscribe({
+      next: (v) => (this.post = v),
+      error: (e) => (this.errorMessage = e.message),
+      complete: () => console.info('complete'),
+    });
+  }
+
+  deletePost(id: number): void {
+    this.jsonPlaceholderService.deletePost(id).subscribe({
+      next: (v) => (this.posts = this.posts.filter((v) => v.id !== id)),
+      error: (e) => (this.errorMessage = e.message),
+      complete: () => console.info('complete'),
+    });
   }
 }
--- sevices/src/data.service.ts
+++ sevices/src/data.service.ts
@@ -1,17 +1,57 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-  getData() {
-    return [
-      { itemId: 1, itemName: 'item 1' },
-      { itemId: 2, itemName: 'item 2' },
-      { itemId: 3, itemName: 'item 3' },
-      { itemId: 4, itemName: 'item 4' },
-    ];
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getPost(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  createPost(post: any): Observable<any> {
+    return this.http.post(this.apiUrl, post, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    });
+  }
+
+  updatePost(id: number, post: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, post, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    });
+  }
+
+  patchPost(id: number, post: any): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${id}`, post, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    });
+  }
+
+  deletePost(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  throwError(error: Error) {
+    return throwError(() => new Error(error.message));
+  }
+
+  getDataFromAPI(): Observable<any[]> {
+    return this.http
+      .get<any[]>('https://jsonplaceholder.typicode.com/posts23')
+      .pipe(catchError(this.throwError));
   }
 }
