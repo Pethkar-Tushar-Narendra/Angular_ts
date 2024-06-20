ccf00248d22927262a273a3b4a8440372d67d52d
Pethkar-Tushar-Narendra <pethkartusharnarendra@gmail.com>
2024-06-20 17:08:34 +0530

    Done with interpolation

--- interpolation/src/app/app.component.ts
+++ interpolation/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'interpolation';
+  title = 'Angular Interpolation Example';
+  user = {
+    name: 'Jane Smith',
+    age: 28,
+  };
+  a = 5;
+  b = 10;
+
+  htmlString = '<script>alert("Hello!")</script>';
+
+  getGreeting(): string {
+    return `Hello, ${this.user.name}!`;
+  }
 }
