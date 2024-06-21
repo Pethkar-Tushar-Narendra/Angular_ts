96beff844409442d4296cf006f0c8f990feac5e3
Pethkar-Tushar-Narendra <pethkartusharnarendra@gmail.com>
2024-06-21 14:49:12 +0530

    Done ParamMap Observable project

--- routing_and_navigations/src/app/item-detail/item-detail.component.ts
+++ routing_and_navigations/src/app/item-detail/item-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-detail',
@@ -7,15 +7,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './item-detail.component.css',
 })
 export class ItemDetailComponent {
-  itemId: string | null = null;
+  itemId: string = '0';
   itemType: string | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    this.itemId = this.route.snapshot.paramMap.get('id');
+    this.route.paramMap.subscribe((params) => {
+      this.itemId = params.get('id') ?? '0';
+    });
     this.route.queryParamMap.subscribe((params) => {
       this.itemType = params.get('type');
+    });
+  }
+  onButtonClick() {
+    this.router.navigate(['/item', this.itemId + 1], {
+      queryParams: {
+        type: 'something different',
+      },
     });
   }
 }
