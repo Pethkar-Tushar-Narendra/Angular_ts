106e211d06369b430172fd677312daafd4c4086f
Pethkar-Tushar-Narendra <pethkartusharnarendra@gmail.com>
2024-06-25 17:47:08 +0530

    Done reading parent params in child route

--- childAndNestedRouting/src/app/user-details/user-details.component.ts
+++ childAndNestedRouting/src/app/user-details/user-details.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.css'
+  styleUrl: './user-details.component.css',
 })
 export class UserDetailsComponent {
+  id: string = '';
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
+  ngOnInit(): void {
+    this.route.parent?.paramMap.subscribe((params: any) => {
+      this.id = params.get('id') ?? '';
+    });
+  }
 }
